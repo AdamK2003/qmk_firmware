5f3c8b2ab652985ef933a3c58480c3238e38fb45
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-03 01:20:46 +0200

    less hackiing

--- keyboards/keychron/k4/rgb/config.h
+++ keyboards/keychron/k4/rgb/config.h
@@ -35,8 +35,6 @@
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 0
 
-// Optimizations for VIA
+// Optimization for VIA
 #define LAYER_STATE_8BIT
-#define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 1200
 
